c546fca6a923dd036158a608edc9de38257e05fa
Elie Saade <saadeelie13@gmail.com>
2024-12-13 13:29:21 +0200

    Add project files.

---
+++ Adv. Project/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Adv._Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
